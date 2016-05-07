20436488154d5a7b46668aa03ae9a28b206fea98
Roberts Mencis <roberts.mencis@gmail.com>
2016-05-08 01:12:19 +0200

    Make grayscale images with identical height

--- preprocess_word_boxes.py
+++ preprocess_word_boxes.py
@@ -1,17 +1,18 @@
 import dirs
 import os
 import shutil
-import cv2
+from PIL import Image
+#import cv2
 
-def preprocess_image(image):
+#def preprocess_image(image):
     # Otsu's thresholding after Gaussian filtering
-    blured_image = cv2.GaussianBlur(image,(5,5),0)
-    histogram,binary_image = cv2.threshold(blured_image,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+    #blured_image = cv2.GaussianBlur(image,(5,5),0)
+    #histogram,binary_image = cv2.threshold(blured_image,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
 
-    return binary_image
+#    return binary_image
 
-source_dir_path = dirs.BASE_DIR_PATH + "/knmp_word_boxes/"
-target_dir_path = dirs.BASE_DIR_PATH + "/knmp_word_boxes_processed/"
+source_dir_path = dirs.KNMP_WORD_BOXES_DIR_PATH
+target_dir_path = dirs.KNMP_PROCESSED_WORD_BOXES_DIR_PATH
 
 # Delete target directory
 if os.path.exists(target_dir_path):
@@ -28,4 +29,20 @@ for source_label_dir_name in [f for f in os.listdir(source_dir_path) if os.path.
     for source_file_name in source_file_names:
         source_file_path = os.path.join(source_label_dir_path, source_file_name)
         target_file_path = os.path.join(target_label_dir_path, source_file_name)
-        shutil.copyfile(source_file_path,target_file_path)
+
+        print(source_file_path)
+
+        image = Image.open(source_file_path)
+        image = image.convert("LA") # Greyscale
+
+        resize_ratio = 0.5
+        fixed_height = 28
+        # Width: average 91.4, std 65.6
+        # Height: average 56.7, std 11.9
+        image = image.resize((int(image.width*resize_ratio),fixed_height))
+
+        #print("%d\t%d" % (image.width,image.height))
+
+        image.save(target_file_path)
+
+        #shutil.copyfile(source_file_path,target_file_path)
