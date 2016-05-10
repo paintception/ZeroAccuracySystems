b5b7cd68601a3f8dbc76402126f67e119b11ce85
Yaroslav Shkarupa <yaroslav@shkarupa.name>
2016-05-10 12:43:11 +0200

    Update structure

--- net/blstm-ctc-net.py
+++ net/blstm-ctc-net.py
@@ -1,15 +1,13 @@
-
-
 import tensorflow as tf
 from tensorflow.models.rnn import rnn, rnn_cell
 from tensorflow.contrib import ctc
 
 import numpy as np
+import datetime
 
-import mnist_input_data
+import rnn_mnist.mnist_input_data as mnist_input_data
+
 mnist = mnist_input_data.read_data_sets("/tmp/data/", one_hot=True)
-
-
 
 # Global parameters
 learning_rate = 0.001
@@ -17,12 +15,12 @@ learning_iterations = 100  # number of mini-batches
 mini_batch_size = 100
 n_hidden_layer = 128
 n_output_classes = 28  # Number of letters in our alphabet
-
+display_time = 5
 
 # Input Data Parameters
 batch_size = 128  # (i, o) Divide input into batches of max input size of i timesteps and max output label size o. If < i or < o should be padded accordingly
 n_input = 8  # Number of input features for each sliding window of 1px
-max_input_timesteps = 50
+max_input_timesteps = 28
 
 # Network weights. Does not depend on batch size
 hidden_weights = tf.Variable(tf.random_normal([n_input, 2 * n_hidden_layer]))  # 2 * n_hidden_layer for forward and backward layer
@@ -31,11 +29,12 @@ hidden_biases = tf.Variable(tf.random_normal([2 * n_hidden_layer]))
 output_weights = tf.Variable(tf.random_normal([2 * n_hidden_layer, n_output_classes]))
 output_biases = tf.Variable(tf.random_normal([n_output_classes]))
 
-
 # Tensorflow graph inout/output
-x = tf.placeholder("float", [None, max_input_timesteps, n_input])
-x_length = tf.placeholder("int32", [None])
-y = tf.placeholder("int32", [None, n_output_classes])
+x = tf.placeholder("float", [batch_size, max_input_timesteps, n_input])
+x_length = tf.placeholder("int32", [batch_size])
+
+y_index = tf.placeholder("int32", [batch_size, max_input_timesteps])
+y_labels = tf.placeholder("int32", [batch_size*max_input_timesteps])
 
 istate_fw = tf.placeholder("float", [None, 2 * n_hidden_layer])
 istate_bw = tf.placeholder("float", [None, 2 * n_hidden_layer])
@@ -45,7 +44,6 @@ istate_bw = tf.placeholder("float", [None, 2 * n_hidden_layer])
 # Used to calculate forward pass of network
 # n_steps - number of timesteps in current batch
 def blstm_layer(_X, _istate_fw, _istate_bw, _x_length):
-
     # input shape: (batch_size, n_steps, n_input)
     _X = tf.transpose(_X, [1, 0, 2])  # permute n_steps and batch_size
     # Reshape to prepare input to hidden activation
@@ -62,30 +60,30 @@ def blstm_layer(_X, _istate_fw, _istate_bw, _x_length):
     _X = tf.split(0, max_input_timesteps, _X)  # n_steps * (batch_size, n_hidden)
 
     # Get lstm cell output
-    outputs = rnn.bidirectional_rnn(lstm_fw_cell, lstm_bw_cell, _X,
-                                            initial_state_fw=_istate_fw,
-                                            initial_state_bw=_istate_bw,
-                                            sequence_length=_x_length
-                                    )
+    outputs, output_state_fw, output_state_bw = rnn.bidirectional_rnn(lstm_fw_cell, lstm_bw_cell, _X,
+                                                                      initial_state_fw=_istate_fw,
+                                                                      initial_state_bw=_istate_bw,
+                                                                      sequence_length=_x_length
+                                                                      )
 
-    # Linear activation
-    # Get inner loop last output
-    # TODO Should be all outputs? Not only last ones
-    return tf.matmul(outputs, output_weights) + output_biases
+    outputs = tf.concat(0, outputs)
+    activation = tf.matmul(outputs, output_weights) + output_biases
+
+    return tf.reshape(activation, [max_input_timesteps, batch_size, n_output_classes])
+
 
 prediction = blstm_layer(x, istate_fw, istate_bw, x_length)
 
-cost = ctc.ctc_loss(prediction, y, x_length)
+cost = ctc.ctc_loss(prediction, tf.SparseTensor(indices=y_index, values=y_labels), x_length)
 optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
 
-correct_pred = tf.equal(tf.argmax(prediction, 1), tf.argmax(y, 1))
-accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
 
 init = tf.initialize_all_variables()
 
 with tf.Session() as sess:
     sess.run(init)
     step = 1
+    prev_output_time = datetime.datetime.now()
     # Keep training until reach max iterations
     while step < learning_iterations:
         batch_xs, batch_ys = mnist.train.next_batch(batch_size)
@@ -95,17 +93,10 @@ with tf.Session() as sess:
         sess.run(optimizer, feed_dict={x: batch_xs, y: batch_ys,
                                        istate_fw: np.zeros((batch_size, 2 * n_hidden_layer)),
                                        istate_bw: np.zeros((batch_size, 2 * n_hidden_layer))})
-        if step % display_step == 0:
-            # # Calculate batch accuracy
-            # acc = sess.run(accuracy, feed_dict={x: batch_xs, y: batch_ys,
-            #                                     istate_fw: np.zeros((batch_size, 2 * n_hidden_layer)),
-            #                                     istate_bw: np.zeros((batch_size, 2 * n_hidden_layer))})
-            # Calculate batch loss
-            loss = sess.run(cost, feed_dict={x: batch_xs, y: batch_ys,
-                                             istate_fw: np.zeros((batch_size, 2 * n_hidden_layer)),
-                                             istate_bw: np.zeros((batch_size, 2 * n_hidden_layer))})
-            print("Iter " + str(step * batch_size) + ", Minibatch Loss= " + "{:.6f}".format(loss) + \
-                  ", Training Accuracy= " + "{:.5f}".format(0))
+        from_prev_output_time = datetime.datetime.now() - prev_output_time
+
+        if step == 1 or from_prev_output_time.seconds > display_time:
+           pass
         step += 1
     print("Optimization Finished!")
     # Calculate accuracy for 128 mnist test images
@@ -115,6 +106,3 @@ with tf.Session() as sess:
     print("Testing Accuracy:", sess.run(accuracy, feed_dict={x: test_data, y: test_label,
                                                              istate_fw: np.zeros((test_len, 2 * n_hidden_layer)),
                                                              istate_bw: np.zeros((test_len, 2 * n_hidden_layer))}))
-
-    
-
