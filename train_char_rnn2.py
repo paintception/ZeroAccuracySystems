9fc82cdca2d4b59bf4ae6f3f0df81605d474fb23
Roberts Mencis <roberts.mencis@gmail.com>
2016-05-12 19:55:40 +0200

    2-layer char RNN (128x128 hidden units), max accuracy 90.64% at 1150720 iteration

--- train_char_rnn2.py
+++ train_char_rnn2.py
@@ -18,7 +18,7 @@ print("Training items:",dataset.get_train_item_count())
 print("Test items:",dataset.get_test_item_count())
 
 # Parameters
-learning_rate = 0.001 
+learning_rate = 0.0005
 print("Learning rate:",learning_rate)
 n_batch_size = 256
 print("Batch size:",n_batch_size)
@@ -32,7 +32,7 @@ n_steps = dataset.get_time_step_count() # Timesteps = image width
 print("Time steps:",n_steps)
 n_cell = 2 # Number of cells/layers
 print("Layers:",n_cell)
-n_hidden = 32 # hidden layer num of features
+n_hidden = 128 # hidden layer num of features
 print("Hidden units:",n_hidden)
 n_classes = dataset.get_class_count() # Classes (A,a,B,b,c,...)
 print("Classes:",n_classes)
