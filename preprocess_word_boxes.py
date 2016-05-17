a8ae975cfe6dce3cc9b799a2d58e6b75a7c7c425
Roberts Mencis <roberts.mencis@gmail.com>
2016-05-17 16:38:58 +0200

    Put preprocessed word files in separate training/test subdirectories

--- preprocess_word_boxes.py
+++ preprocess_word_boxes.py
@@ -2,20 +2,41 @@ import dirs
 import os
 import shutil
 from PIL import Image
+import random
 
 source_dir_path = dirs.KNMP_WORD_BOXES_DIR_PATH
 target_dir_path = dirs.KNMP_PROCESSED_WORD_BOXES_DIR_PATH
 
-# Delete target directory
+train_dir_path = os.path.join(target_dir_path,"train")
+test_dir_path = os.path.join(target_dir_path,"test")
+
+train_ratio = 0.9
+
+# Delete and create target directories
 if os.path.exists(target_dir_path):
     shutil.rmtree(target_dir_path)
-os.makedirs(target_dir_path)
+
+os.makedirs(train_dir_path)
+os.makedirs(test_dir_path)
 
 max_width = 0
 source_file_names = [f for f in os.listdir(source_dir_path) if f.endswith(".png")]
+random.shuffle(source_file_names)
+
+file_counter = 0
+train_file_count = int(len(source_file_names) * train_ratio)
 for source_file_name in source_file_names:
+    file_counter += 1
+    train = True
+    test = False
+    if (file_counter > train_file_count):
+        train = False
+        test = True
+
     source_file_path = os.path.join(source_dir_path, source_file_name)
-    target_file_path = os.path.join(target_dir_path, source_file_name)
+    target_file_path = os.path.join(train_dir_path, source_file_name)
+    if test:
+        target_file_path = os.path.join(test_dir_path, source_file_name)
 
     print(target_file_path)
 
