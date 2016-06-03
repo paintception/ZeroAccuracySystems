e57490c23fa54a162fef6296b0403609fb7267df
Yaroslav Shkarupa <yaroslav@shkarupa.name>
2016-06-03 17:17:26 +0200

    Recognizer

---
+++ recognizer2.py
@@ -0,0 +1,51 @@
+import argparse
+from PIL import Image
+from toolbox import wordio
+import prepare_features as pf
+
+from recognizer_seq2seq import recognize_seq2seq
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Database initialization')
+    group = parser.add_mutually_exclusive_group()
+
+    group.add_argument('-s', '--seq2seq', dest='seq2seq', action='append', nargs=3, metavar=('INPUT_IMAGE', 'INPUT_WORDS', 'OUTPUT_WORDS'),
+                       help='Use seq2seq model for recognition')
+
+    group.add_argument('-c', '--ctc', dest='ctc', action='append', nargs=3, metavar=('INPUT_IMAGE', 'INPUT_WORDS', 'OUTPUT_WORDS'),
+                       help='Use ctc model for recognition')
+
+    input_args = parser.parse_args()
+
+    if input_args.seq2seq:
+        n_image_rnn_steps = 50
+        text_lines, word_boxes, images_data = prepare_data(input_args.seq2seq[0][0], input_args.seq2seq[0][1], n_image_rnn_steps)
+        recognize_seq2seq(images_data, word_boxes, text_lines, input_args.seq2seq[0][2], n_image_rnn_steps)
+
+
+
+def prepare_data(page_file_path, input_words_file_path, n_image_rnn_steps):
+    # Read PPM
+    page_image = Image.open(page_file_path)
+    print("Page image size:", page_image.width, "x", page_image.height)
+
+    # Read .words file to get word boxes
+    text_lines, image_name = wordio.read(input_words_file_path)
+
+    word_boxes = []
+    images_data = []
+    for text_line in text_lines:
+        for word_box in text_line:
+            word_boxes.append(word_box)
+            box = (word_box.left, word_box.top, word_box.right, word_box.bottom)
+            word_image = page_image.crop(box)
+            preprocessed_word_image = pf.preprocess_image(word_image)
+            image_data = pf.get_feature_data_for_image(preprocessed_word_image)
+            image_data = pf.get_data_with_fixed_time_step_count(image_data, n_image_rnn_steps)
+            images_data.append(image_data)
+
+    return text_lines, word_boxes, images_data
+
+if __name__ == "__main__":
+    parse_args()
---
+++ recognizer_seq2seq.py
@@ -0,0 +1,56 @@
+import sys
+from PIL import Image
+from toolbox import wordio
+import prepare_features as pf
+import blstm_seq2seq.rnn_seq2seq_model as model
+import tensorflow as tf
+
+
+def recognize_seq2seq(images_data, word_boxes, text_lines, output_words_file_path, n_rnn_steps):
+
+    # Parameters
+    model_file_path = "./models/knmp_seq2seq.model" # Assume current directory
+    # model_file_path = "/Users/rmencis/RUG/Handwriting_Recognition/models/KNMP/last.model" # Assume current directory
+    n_image_rnn_steps = n_rnn_steps # Max width of word box
+    n_image_features = pf.FEATURE_COUNT
+    n_label_rnn_steps = 10 # Max number of characters in label
+    unique_chars = [' ', '!', '#', '%', '&', 'A', 'B', 'C', 'D', 'G', 'H', 'I', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'x', 'y']
+    n_classes = len(unique_chars)
+
+    print("Model path:",model_file_path)
+    print("Maximal number of timesteps:",n_image_rnn_steps)
+    print("Features:",n_image_features)
+    print("Maximal number of characters in a word:",n_label_rnn_steps)
+    print("Classes:",n_classes)
+
+    # Define RNN
+    print("Initializing RNN...")
+    image_rnn_input_data = tf.placeholder("float", [None, n_image_rnn_steps, n_image_features]) # (n_batch_size, n_steps, n_features)
+    label_rnn_input_data = tf.placeholder("float", [None, n_label_rnn_steps, n_classes]) # (n_batch_size,n_label_rnn_steps)
+
+    label_rnn_outputs,label_rnn_predicted_index_labels = model.define_seq2seq_rnn_for_prediction(image_rnn_input_data,label_rnn_input_data)
+
+    # Initialize session
+    sess = tf.InteractiveSession()
+
+    # Initiale the variables
+    init = tf.initialize_all_variables()
+    sess.run(init)
+
+    restore_saver = tf.train.Saver()
+    restore_saver.restore(sess, model_file_path)
+
+    print("Recognizing...")
+    predicted_train_sample_text_labels = model.get_label_rnn_result(label_rnn_predicted_index_labels,
+                                                                    image_rnn_input_data, label_rnn_input_data,
+                                                                    unique_chars, images_data)
+
+    for word_box,predicted_train_sample_text_label in zip(word_boxes,predicted_train_sample_text_labels):
+        predicted_text = predicted_train_sample_text_label.strip()
+        print("Word box:",(word_box.left, word_box.top, word_box.right, word_box.bottom),"Correct word:",word_box.text,"Predicted word:",predicted_text)
+        word_box.text = predicted_text
+
+    # Save output .words file
+    wordio.save(text_lines, output_words_file_path)
+
+    print("Finished.")
