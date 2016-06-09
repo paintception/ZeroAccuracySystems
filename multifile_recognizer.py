939d348503e7cb56616bea2f91dee8b5a0b3cb00
Roberts Mencis <roberts.mencis@gmail.com>
2016-06-09 18:37:28 +0200

    Multi-file recognizer

---
+++ multifile_recognizer.py
@@ -0,0 +1,20 @@
+import os
+import subprocess
+
+pages_dir_path = "/Users/rmencis/RUG/Handwriting_Recognition/subsettest"
+words_dir_path = "/Users/rmencis/RUG/Handwriting_Recognition/subsettest"
+output_dir_path = "/Users/rmencis/RUG/Handwriting_Recognition/subsettest/output/ctc"
+
+page_file_names = [f for f in os.listdir(pages_dir_path) if f.endswith(".ppm")]
+
+for page_file_name in page_file_names:
+    print(page_file_name)
+
+    words_file_name = page_file_name.replace(".ppm",".words")
+
+    page_file_path = os.path.join(pages_dir_path,page_file_name)
+    words_file_path = os.path.join(words_dir_path,words_file_name)
+    output_file_path = os.path.join(output_dir_path,words_file_name)
+
+    subprocess.run(["python3", "recognizer.py","-c",page_file_path, words_file_path, output_file_path])
+
